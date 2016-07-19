4d34ae9634909f9ef251d211f1607821fdf33494
HusseinReda <hussein.abumaash@gmil.com>
2016-07-19 18:53:19 +0200

    review server added

---
+++ review_service/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ReviewService::Application.config.secret_token = '24c6e6437311511008c479416d2df0c01eadad26d7f0556c8e2378e03894b3592f85f546476a76307ca48fa4ad9282483ce089cd010168ab9509e88fa31369a9'
